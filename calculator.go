6f0d889ebcc3f22709953e2884edc759eeb66f38
Daniel Oliver <danoliver@bc.com>
2020-09-27 21:09:00 -0600

    Finished book.  Almost all stretch goals

--- calculator.go
+++ calculator.go
@@ -1,13 +1,50 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import (
+	"errors"
+	"math"
+)
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
-	return b - a
+	return a - b
+}
+
+// Multiply takes a variatic parameter of numbers and returns the result of multiplying them.
+func Multiply(nums ...float64) float64 {
+
+	if len(nums) == 0 {
+		return 0.0
+	} else if len(nums) == 1 {
+		return nums[0]
+	} else {
+		total := nums[0]
+		for _, num := range nums[1:] {
+			total = total * num
+		}
+		return total
+	}
+}
+
+// Divide returns the result of dividing b from a
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("divide by zero error")
+	}
+	return a / b, nil
+}
+
+// Sqrt return the square root of a number
+func Sqrt(a float64) (float64, error) {
+	if a < 0 {
+		return 0, errors.New("number cannot be less than zero")
+	}
+	return math.Sqrt(a), nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -5,20 +5,93 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	a, b        float64
+	want        float64
+	errExpected bool
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{b: 5, want: 5},
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{a: 5, b: 4, want: 1},
+		{a: 1, b: -1, want: 2},
+		{a: -4, b: 5.5, want: -9.5},
+	}
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{a: 5, b: 4, want: 20},
+		{a: 1, b: -1, want: -1},
+		{a: -4},
+		{a: -4, b: 4, want: -16},
+	}
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{5, 2, 2.5, false},
+		{25, 5, 5, false},
+		{444, 0, 0, true},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		if err == nil && tc.errExpected {
+			t.Fatalf("Divide(%f, %f): error expected but got %f", tc.a, tc.b, got)
+		} else if err != nil && !tc.errExpected {
+			t.Fatalf("Divide(%f, %f): error not expected but got %v", tc.a, tc.b, err)
+		} else if !tc.errExpected && tc.want != got {
+			t.Errorf("Divide(%f, %f): want %f, got %f, error? %v", tc.a, tc.b, tc.want, got, err)
+		}
+	}
+}
+
+func TestSqrt(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{a: 16, want: 4, errExpected: false},
+		{a: 64, want: 8, errExpected: false},
+		{a: 0, want: 0, errExpected: false},
+		{a: -16, want: 0, errExpected: true},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Sqrt(tc.a)
+		if err == nil && tc.errExpected {
+			t.Fatalf("Sqrt(%f): error expected but got %f", tc.a, got)
+		} else if err != nil && !tc.errExpected {
+			t.Fatalf("Sqrt(%f): error not expected but got %v", tc.a, err)
+		} else if !tc.errExpected && tc.want != got {
+			t.Errorf("Sqrt(%f): want %f, got %f, error? %v", tc.a, tc.want, got, err)
+		}
+	}
+}
